fe81185a453486e88a219e7114500fd00645dfbc
ElisaTojin <elisactojin@gmail.com>
2022-11-04 11:42:19 -0700

    added tiles
    Added movement to the ruby character.
    Added 10 of the 55 tiles.

--- RubysAdventureTojin/Assets/Scripts/RubyController.cs
+++ RubysAdventureTojin/Assets/Scripts/RubyController.cs
@@ -13,8 +13,19 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //create 2 variables to use the unity built in axes
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //Create my movement vector
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+
+        //Create the movement of my character
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        //set the new position
         transform.position = position;
     }
 }
