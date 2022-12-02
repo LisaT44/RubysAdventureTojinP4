f1883eae6974bd081bc54a0dd8d6f5658336497f
ElisaTojin <elisactojin@gmail.com>
2022-12-02 11:47:19 -0800

    Worked on project
    I finished ruby's controller script.
    I added new prefabs to decorate my world and I added a new script.

--- RubysAdventureTojin/Assets/Scripts/RubyController.cs
+++ RubysAdventureTojin/Assets/Scripts/RubyController.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         //create 2 variables to use the unity built in axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+       horizontal = Input.GetAxis("Horizontal");
+       vertical = Input.GetAxis("Vertical");
+    }
 
+    void FixedUpdate()
+    {
         //Create my movement vector
-        Vector2 position = transform.position;
-
+        Vector2 position = rigidbody2d.position;
         //Create the movement of my character
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //set the new position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
