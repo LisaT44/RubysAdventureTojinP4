f1ba054e4d1f5b39687202a854fceccc2e6216b0
ElisaTojin <elisactojin@gmail.com>
2022-12-05 17:37:53 -0800

    I worked on Ruby's adventure.
    I worked on the collectibles, damage zones, enemies, and the animations.

--- RubysAdventureTojin/Assets/Scripts/RubyController.cs
+++ RubysAdventureTojin/Assets/Scripts/RubyController.cs
@@ -7,7 +7,13 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health {  get { return currentHealth; }}
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -27,6 +33,13 @@ public class RubyController : MonoBehaviour
         //create 2 variables to use the unity built in axes
        horizontal = Input.GetAxis("Horizontal");
        vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -34,14 +47,24 @@ public class RubyController : MonoBehaviour
         //Create my movement vector
         Vector2 position = rigidbody2d.position;
         //Create the movement of my character
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
