7654349f681e1a6f102779c450491f5d473a3e75
ElisaTojin <elisactojin@gmail.com>
2022-12-06 16:56:42 -0800

    I worked on the projectiles
    I worked on the projectiles script and the animations for the fixed robot.

--- RubysAdventureTojin/Assets/Scripts/Projectile.cs
+++ RubysAdventureTojin/Assets/Scripts/Projectile.cs
@@ -7,8 +7,7 @@ public class Projectile : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
@@ -21,12 +20,19 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
         {
-            Debug.Log("Projectile Collision with" + other.gameObject);
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
             Destroy(gameObject);
         }
 
